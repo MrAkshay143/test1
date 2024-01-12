9a4e83827d6c323bfdb8d157afb3b9ad3005881d
MrAkshay143 <contact@imakshay.in>
2024-01-12 15:34:39 +0530

    Updates

---
+++ dist/src/Data/portfolio.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        title: "Weather App",
+        imgUrl: "assets/WeatherApp.jpg",
+        stack: ["Html", "Css", "JavaScript"],
+        link: "https://souravmandal2004.github.io/WeatherApp"
+    },
+    {
+        title: "Password Generator",
+        imgUrl: "assets/PasswordGenerator.jpg",
+        stack: ["Html", "Css", "JavaScript"],
+        link: "https://souravmandal2004.github.io/weatherApp/"
+    },
+    {
+        title: "Share Modal",
+        imgUrl: "assets/shareModal.jpg",
+        stack: ["Html", "Css", "JavaScript"],
+        link: "https://souravmandal2004.github.io/shareModal"
+    },
+
+]
